afd175c7ba67baf0c9842ffca81786cd2484f8c4
AlessandroRonconi <ronconi.alessandro46@gmail.com>
2026-05-19 17:09:27 +0200

    new file:   progetto-personale-siw/.gitattributes 	new file:   progetto-personale-siw/.gitignore 	new file:   progetto-personale-siw/.mvn/wrapper/maven-wrapper.properties 	new file:   progetto-personale-siw/mvnw 	new file:   progetto-personale-siw/mvnw.cmd 	new file:   progetto-personale-siw/pom.xml 	new file:   progetto-personale-siw/src/main/java/it/uniroma3/siw/progetto_personale_siw/ProgettoPersonaleSiwApplication.java 	new file:   progetto-personale-siw/src/main/resources/application.properties 	new file:   progetto-personale-siw/src/test/java/it/uniroma3/siw/progetto_personale_siw/ProgettoPersonaleSiwApplicationTests.java

---
+++ progetto-personale-siw/src/main/java/it/uniroma3/siw/progetto_personale_siw/ProgettoPersonaleSiwApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.progetto_personale_siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoPersonaleSiwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoPersonaleSiwApplication.class, args);
+	}
+
+}
---
+++ progetto-personale-siw/src/test/java/it/uniroma3/siw/progetto_personale_siw/ProgettoPersonaleSiwApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.progetto_personale_siw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoPersonaleSiwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
