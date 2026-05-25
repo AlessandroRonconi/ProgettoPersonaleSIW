dc9ae9b181bf55bb99f7ced5478dde190dd355ae
Francesc <chicco.salvati@gmail.com>
2026-05-25 16:53:49 +0200

    new file:   progetto-personale-siw/src/main/java/it/uniroma3/siw/progetto_personale_siw/controller/GlobalController.java 	modified:   progetto-personale-siw/src/main/resources/templates/commenti/list.html 	modified:   progetto-personale-siw/src/main/resources/templates/corsi/list.html 	modified:   progetto-personale-siw/src/main/resources/templates/index.html 	modified:   progetto-personale-siw/src/main/resources/templates/login.html 	modified:   progetto-personale-siw/src/main/resources/templates/register.html

---
+++ progetto-personale-siw/src/main/java/it/uniroma3/siw/progetto_personale_siw/controller/GlobalController.java
@@ -0,0 +1,22 @@
+package it.uniroma3.siw.progetto_personale_siw.controller;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalController { //Serve per avere sempre disponibile l’utente loggato nelle pagine HTML
+  //UserDetails è l’utente loggato visto da Spring Security
+  @ModelAttribute("userDetails")
+  public UserDetails getUser() {
+    UserDetails user = null;
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (!(authentication instanceof AnonymousAuthenticationToken)) {
+      user = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+    return user;
+  }
+}
+
